c21985167d3e7f1f807cfdadc404aee02cd85ebc
Yokiijay <yokiijay@gmail.com>
2020-02-15 21:46:13 +0800

    change input path

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   input: 'src/index.js',
   output: {
-    file: 'bundle.js',
+    file: './lib/bundle.js',
     format: 'cjs'
   },
   plugins: [
