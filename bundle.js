161c3fad8b8a10af5a1f5f167a259ca587505b7e
Yokiijay <yokiijay@gmail.com>
2020-02-15 17:52:07 +0800

    without config

---
+++ bundle.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var foo = 'hello world';
+
+exports.foo = foo;
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button(){
+  return(
+    <button>Button</button>
+  )
+}
+
+export default Button
